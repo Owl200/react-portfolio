924a9719412671c42f60cbe32ae1772ab9b249c3
Owl200 <alromaru@hotmail.com>
2022-06-27 01:39:06 -0500

    fixed menu buttons

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,8 @@
 import React from 'react'
-// import button1 from '../images/button-1.png'
-// import button2 from '../images/button-2.png'
-// import button3 from '../images/button-3.png'
+import { motion } from 'framer-motion'
+import button1 from '../images/button-1.png'
+import button2 from '../images/button-2.png'
+import button3 from '../images/button-3.png'
 import '../styles/menu.css'
 
 const Menu = () => {
@@ -9,21 +10,24 @@ const Menu = () => {
     <div className='menu'>
 
         <div className='menu-button__div'>
-            <div className='menu-button__div-img'>
+            <motion.div whileHover={{scale: 1.5}} className='menu-button__div-img'>
+                <img src={button1} alt='about button' />
                 <p>About</p>
-            </div>
+            </motion.div>
         </div>
 
         <div className='menu-button__div'>
-            <div className='menu-button__div-img'>
+            <motion.div whileHover={{scale: 1.5}} className='menu-button__div-img'>
+                <img src={button2} alt='projects button' />
                 <p>Projects</p>
-            </div>
+            </motion.div>
         </div>
         
         <div className='menu-button__div'>
-            <div className='menu-button__div-img'>
+            <motion.div whileHover={{scale: 1.5}} className='menu-button__div-img'>
+                <img src={button3} alt='contact button' />
                 <p>Contact Me</p>
-            </div>
+            </motion.div>
         </div>
 
     </div>
