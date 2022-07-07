4239df254e49241d4f6a82a4dd60658b55e413c8
Owl200 <alromaru@hotmail.com>
2022-07-06 22:58:46 -0500

    improved responsiveness on menu buttons

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React, { useState, useEffect } from 'react'
+import { motion, useCycle } from 'framer-motion'
 import button1 from '../images/button-1.svg'
 import button2 from '../images/button-2.svg'
 import button3 from '../images/button-3.svg'
@@ -7,7 +7,13 @@ import '../styles/menu.css'
 
 
 const Menu = ({section}) => {
-    console.log(section)
+    const [windowSize, setWindowSize] = useState(window.innerWidth)
+    const [isOpen, toggleOpen] = useCycle(false, true)
+
+    useEffect( _ => {
+        window.addEventListener('resize', _ => setWindowSize(window.innerWidth))
+      }, [windowSize])
+
 
     const handleClick = (name) => {
         section(name)
@@ -17,28 +23,36 @@ const Menu = ({section}) => {
   return (
     <div className='menu'>
 
+        {windowSize > 600 ?
+        <>
         <div className='menu-button__div' onClick={_ => handleClick('about')} >
-            <motion.div whileHover={{scale: 1.5}} className='menu-button__div-img' >
-                <svg  viewBox="0 0 380 72" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M4 61L25 3H375L337 69.4133L4 61Z" fill="black" stroke="white" stroke-width="5"/>
-                </svg>
+            <motion.div whileHover={{scale: 1.5}} whileTap={{scale: 1.5}} className='menu-button__div-img' >
+                <img src={button1} alt='projects button'/>
                 <p>About</p>
             </motion.div>
         </div>
 
         <div className='menu-button__div' onClick={_ => handleClick('project')}>
-            <motion.div whileHover={{scale: 1.5}} className='menu-button__div-img' >
+            <motion.div whileHover={{scale: 1.5}} whileTap={{scale: 1.5}} className='menu-button__div-img' >
                 <img src={button2} alt='projects button'/>
                 <p>Projects</p>
             </motion.div>
         </div>
         
         <div className='menu-button__div' onClick={_ => handleClick('contact')}>
-            <motion.div whileHover={{scale: 1.5}} className='menu-button__div-img' >
+            <motion.div whileHover={{scale: 1.5}} whileTap={{scale: 1.5}} className='menu-button__div-img' >
                 <img src={button3} alt='contact button'/>
                 <p>Contact Me</p>
             </motion.div>
-        </div>
+        </div> 
+        </>
+        :
+        <div className='menu-button__div' onClick={toggleOpen}>
+            <motion.div whileHover={{scale: 1.5}} whileTap={{scale: 1.5}} className='menu-button__div-img' >
+                <img src={button1} alt='projects button'/>
+                {isOpen ? <p>Menu   ▼</p> : <p>Menu   ▲</p>}
+            </motion.div>
+        </div>}
 
     </div>
   )
